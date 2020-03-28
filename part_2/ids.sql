8b9a93add283b882cea528b4f273d2f1f4607385
hojkas <ivka.strnadova@gmail.com>
2020-03-28 21:59:49 +0100

    first two

--- part_2/ids.sql
+++ part_2/ids.sql
@@ -1,26 +1,50 @@
 -- autori: xstrna14, xlebod00
 -- zadani: 24 Videopujcovna
 
-drop table something;
-drop table something2;
+-- drop predchozich tabulek, pro testovaci ucely
+--TODO comment
 
-create table something
-(
-	value int generated as identity
-		constraint something_pk
-			primary key,
-	name varchar(100)
-);
-create table something2
-(
-	value int generated as identity
-		constraint something2_pk
-			primary key,
-	name varchar(100)
-);
-INSERT INTO something (name)
-VALUES ('hello');
-INSERT INTO something (name)
-VALUES ('hello');
+drop table title;
+drop table copy;
 
-SELECT * from something;
+-- create
+create table title
+(
+	title_id int generated as identity
+		constraint title_pk
+			primary key,
+	name varchar(50) not null,
+	year int not null,
+	price_per_day float not null
+);
+create table copy
+(
+	copy_id int generated as identity,
+	title_id int not null,
+	constraint copy_pk
+		primary key (copy_id, title_id),
+	constraint copy_title_fk
+		foreign key (title_id) references title (title_id)
+);
+
+
+
+
+
+
+
+
+-- INSERT
+-- naplneni tabulek vzorovymi daty
+
+INSERT INTO title (name, year, price_per_day)
+VALUES ('Vecny pribeh', 1980, 50);
+INSERT INTO title (name, year, price_per_day)
+VALUES ('Vecny pribeh', 1990, 50);
+insert into copy (title_id)
+values(1);
+
+
+-- kontroni vypisy, TODO vymazat
+SELECT * from title;
+SELECT * from copy;
