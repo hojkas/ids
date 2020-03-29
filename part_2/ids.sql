2b9a47c5d30edf3a2521c2862b440e6b9ac69ef7
hojkas <ivka.strnadova@gmail.com>
2020-03-29 16:07:44 +0200

    backup

--- part_2/ids.sql
+++ part_2/ids.sql
@@ -4,10 +4,19 @@
 -- drop predchozich tabulek, pro testovaci ucely
 --TODO comment
 
-drop table title;
-drop table copy;
+drop table title cascade constraints;
+drop table copy cascade constraints;
+drop table borrow cascade constraints;
+drop table genre cascade constraints;
+drop table person cascade constraints;
 
 -- create
+create table genre
+(
+    name varchar(50) not null
+        constraint genre_pk
+            primary key
+);
 create table title
 (
 	title_id int generated as identity
@@ -15,6 +24,9 @@ create table title
 			primary key,
 	name varchar(50) not null,
 	year int not null,
+	genre varchar(50) not null,
+	constraint genre_fk
+	    foreign key (genre) references genre(name),
 	price_per_day float not null
 );
 create table copy
@@ -26,25 +38,93 @@ create table copy
 	constraint copy_title_fk
 		foreign key (title_id) references title (title_id)
 );
+create table person
+(
+    person_id int generated as identity
+        constraint person_pk
+            primary key,
+    first_name varchar(50) not null,
+    last_name varchar(50) not null,
+    house_number int not null,
+    street varchar(50) not null,
+    town varchar(50) not null,
+    zip_code int not null,
+    constraint zip_check
+        check(regexp_like(zip_code, '^\d{5}$'))
+);
+create table customer
+(
+    customer_id int generated as identity
+        constraint customer_pk
+            primary key,
+    person_id not null,
+    constraint customer_person_fk
+        foreign key (person_id) references person(person_id)
+)
+create table employee
+(
+    employee_id int generated as identity
+        constraint employee_pk
+            primary key,
+    person_id not null,
+    constraint employee_person_fk
+        foreign key (person_id) references person(person_id)
+)
+create table borrow
+(
+    borrow_id int generated as identity
+        constraint borrow_pk
+            primary key,
+    borrow_date date not null,
+    return_date date,
+    price float,
+    customer_id int not null, --borrowed title
+    employee_id int not null, -- checked it out
+    constraint customer_id_fk
+        foreign key (customer_id) references customer(customer_id),
+    constraint employee_id_fk
+        foreign key (employee_id) references employee(employee_id)
+    --TODO constraint user is user and empl is empl
+);
 
-
-
-
-
-
+-- dodatecna omezeni
 
 
 -- INSERT
 -- naplneni tabulek vzorovymi daty
 
-INSERT INTO title (name, year, price_per_day)
-VALUES ('Vecny pribeh', 1980, 50);
-INSERT INTO title (name, year, price_per_day)
-VALUES ('Vecny pribeh', 1990, 50);
+insert into genre (name)
+values ('romance');
+insert into genre (name)
+values ('thriller');
+
+INSERT INTO title (name, year, genre, price_per_day)
+VALUES ('Vecny pribeh', 1980, 'romance', 50);
+INSERT INTO title (name, year, genre, price_per_day)
+VALUES ('Vecny pribeh', 1990, 'thriller', 50);
 insert into copy (title_id)
 values(1);
 
+insert into person(first_name, last_name, house_number, street, town, zip_code)
+values ('Jiri', 'Novak', 58, 'Netinska', 'Brno', 58000);
+insert into customer(person_id)
+values(1);
+insert into person(first_name, last_name, house_number, street, town, zip_code)
+values ('Eva', 'Novotna', 240, 'Brnenska', 'Boskovice', 54110);
+insert into employee(person_id)
+values(2);
+insert into person(first_name, last_name, house_number, street, town, zip_code)
+values ('Eva', 'Novotna', 240, 'Brnenska', 'Boskovice', 54110);
+insert into employee(person_id)
+values(3);
+insert into customer(person_id)
+values(3);
 
 -- kontroni vypisy, TODO vymazat
 SELECT * from title;
 SELECT * from copy;
+SELECT * from genre;
+SELECT * from person;
+SELECT * from employee;
+SELECT * from customer;
+SELECT * from borrow;
